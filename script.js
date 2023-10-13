b74798cee784eceee425c6d421d20f26994dc2d2
JosieEsi <josephine.kpodo@amalitech.org>
2023-10-13 16:03:56 +0000

    updated the javascript code for the todo-app

--- script.js
+++ script.js
@@ -33,64 +33,180 @@ todoform.addEventListener('keydown', (event) => {
 })
 
 let todos = [{
-  name: "Complete online JavaScript course"},
+  name: "Complete online JavaScript course",
+  completed: true},
   {
-  name: "Jog around the park 3x"},
+  name: "Jog around the park 3x",
+  completed: false},
   {
-    name: "10 minutes meditation"},
+    name: "10 minutes meditation",
+    completed: false},
   {
-    name: "Read for 1 hour"},
+    name: "Read for 1 hour",
+    completed: false},
   {
-    name: "Pick up groceries"},
+    name: "Pick up groceries",
+    completed: false},
   {
-    name: "Conplete ToDo App on Frontend Mentor"}     
+    name: "Complete ToDo App on Frontend Mentor",
+    completed: false}     
   
 ];
-displayTodo;
+
+
+displayTodo();
 
 
 
 function displayTodo(){
 
   let theTodoList = '';
-
-
-
- 
+  let incompleteCount = 0;
+  
 
   todos.forEach(function(toDo, index){
     const name = toDo.name;
+    const completed = toDo.completed;
+    const completedClass = completed ? 'completed-task' : '';
     const disp = `
     <div class="new-todo">
-    <input class="todo-check" type="checkbox">
-    <div>${name}</div>
+    <input class="todo-check" type="checkbox" data-index="${index}" ${completed  ? 'checked' : ''}>
+    <div class="${completedClass}">${name}</div>
     <button class="delete-todo">&times</button>
     </div>
     `
     ;
 
     theTodoList += disp;
-  });
 
+    if(!completed){
+      incompleteCount++
+    }
+  
+});
+
+const filterCountSection = `
+<div class="endtext">
+  <span id="items-left">${incompleteCount}</span> items left
+  <button class="all">All</button>
+  <button class="active">Active</button>
+  <button class="completed">Completed</button>
+  <button class="clear-completed">Clear Completed</button>
+</div>
+`;
+
+
+
+theTodoList += filterCountSection;
+
+
+  
+
+  // console.log(theTodoList);
 
   document.querySelector('.todo-list').innerHTML = theTodoList;
-
-  console.log(theTodoList);
-  
 
   const deleteTodoButtons = document.querySelectorAll('.delete-todo');
   deleteTodoButtons.forEach((deleteButton, index) => {
     deleteButton.addEventListener('click', () => {
       todos.splice(index, 1);
       displayTodo();
+
+
     })
   })
+
+  activeFilterButtons();
+  activateCheckbox();
+}
+function activateCheckbox (){
+const checkboxes = document.querySelectorAll('.todo-check');
+checkboxes.forEach((checkbox, index) => {
+  checkbox.addEventListener('change', (event) => {
+    const index = event.target.getAttribute('data-index');
+    const toDo = todos[index]
+    toDo.completed = checkbox.checked;
+    displayTodo();
+  });
+});
+}
+
+function activeFilterButtons (){
+const showAll = document.querySelector('.all')
+showAll.addEventListener('click', () => {
+  displayTodo();
+  console.log(showAll);
+})
+
+
+const showActive = document.querySelector('.active')
+showActive.addEventListener('click', () => {
+const activeTodos = todos.filter(todo => !todo.completed)
+  displayFilteredTodo(activeTodos)
+
+})
+
+const showCompleted = document.querySelector('.completed')
+showCompleted.addEventListener('click', () => {
+const completedTodos = todos.filter(todo => todo.completed)
+displayFilteredTodo(completedTodos)
+
+})
+
+const clearCompleted = document.querySelector('.clear-completed')
+clearCompleted.addEventListener('click', () => {
+todos = todos.filter(todo => !todo.completed);
+displayTodo();
+})
+}
+
+function displayFilteredTodo (filteredTodos) {
+  let theTodoList = '';
+
+  filteredTodos.forEach(function (toDo, index) {
+    const name = toDo.name;
+    const completed = toDo.completed;
+    const completedClass = completed ? 'completed-task' : '';
+    const disp =`
+    <div class="new-todo">
+    <input class="todo-check" type="checkbox" data-index="${index}" ${completed  ? 'checked' : ''}>
+    <div class="${completedClass}">${name}</div>
+    <button class="delete-todo">&times</button>
+    </div>
+    `;
+
+    theTodoList += disp;
+
+  });
+
+  const filterCountSection = `
+<div class="endtext">
+  <span id="items-left">${todos.length}</span> items left
+  <button class="all">All</button>
+  <button class="active">Active</button>
+  <button class="completed">Completed</button>
+  <button class="clear-completed">Clear Completed</button>
+</div>
+`;
+
+
+
+theTodoList += filterCountSection;
+
+  document.querySelector('.todo-list').innerHTML = theTodoList;
+
+  activeFilterButtons();
+  activateCheckbox();
 }
 
 function addToDo (){
 
    const toDoInput = document.querySelector('.todo-tab');
    const name = toDoInput.value;
+
+
+
+
 
    todos.push({name});
    console.log(name);
@@ -100,9 +216,6 @@ function addToDo (){
 
  
 }
-
-// console.log(addToDo);
-//   console.log(displayTodo);
 
 
 
